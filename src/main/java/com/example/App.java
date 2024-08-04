b1e6d2a1e24fd89dd4be5a92ed1ecc8bcbe6bc09
Ali Tajeldin <alitajeldin@gmail.com>
2024-08-03 17:26:16 -0700

    implement command line options

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,12 +9,27 @@ import java.util.Random;
  */
 public class App {
     private static void generateFile(String fileName, int size, int numPositive) throws IOException {
+        System.out.println("Generate File: " + fileName);
+
         Cache c = new Cache(size);
         Random rand = new Random();
         for (int i = 0; i < numPositive; ++i) {
             c.set(Encoder.numIdToStr(rand.nextInt(size)));
+            c.set("000AA");
         }
         c.save(fileName);
+    }
+
+    private static void readFile(String fileName, int size) throws IOException {
+        System.out.println("Read File: " + fileName);
+
+        long startTime = System.nanoTime();
+        Cache c = new Cache(fileName, size);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+
+        System.out.println("Duration (nano): " + Long.toString(duration));
+        System.out.println(c.has("000AA"));
     }
 
     public static void main(String[] args) throws IOException {
@@ -23,14 +38,12 @@ public class App {
 
         if (args.length > 0) {
             if (args[0].equals("-g")) {
-                System.out.println("Generate File: " + fileName);
                 App.generateFile(fileName, fileSize, fileSize / 10);
             } else if (args[0].equals("-r")) {
-                System.out.println("read");
+                App.readFile(fileName, fileSize);
             } else {
-                System.out.println("unknown");
+                System.out.println("unknown argument");
             }
         }
-
     }
 }
